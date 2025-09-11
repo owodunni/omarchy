efdaa29abd0cb4ff01ec8a663d81694670ea5cd3
Alexander Poole <alex.o.poole@gmail.com>
2025-09-11 10:34:43 +0200

    Add keybinding for slack

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -13,3 +13,4 @@ omarchy-webapp-install "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-lab
 omarchy-webapp-install "Figma" https://figma.com/ https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
 omarchy-webapp-install "Discord" https://discord.com/channels/@me https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png
+omarchy-webapp-install "Slack" https://app.slack.com/client/T02AA1BK1/D02KWLE7V8X https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/slack.png
